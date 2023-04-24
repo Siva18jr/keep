386eeefe74ec833d5e0fa652b2b16d1a9bbb5fda
Siva <108004062+Siva18jr@users.noreply.github.com>
2023-04-24 23:37:35 +0530

    theme change by using local storage

--- Js/home.js
+++ Js/home.js
@@ -283,3 +283,30 @@ function restore(){
   }
 
 }
+
+//theme
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'moon' : 'sun'
+
+if(selectedTheme){
+
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'moon' ? 'add' : 'remove'](iconTheme)
+
+}
+
+themeButton.addEventListener('click', function(){
+
+  document.body.classList.toggle(darkTheme);
+  // themeButton.classList.toggle(iconTheme);
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+
+});
